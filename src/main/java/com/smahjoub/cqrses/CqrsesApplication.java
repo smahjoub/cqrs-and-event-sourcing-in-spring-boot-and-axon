a88682e5419b1050a0d3ad5bd508fae46f18379b
Saifeddine MAHJOUB <saifeddine.mahjoub@outlook.com>
2021-01-24 21:24:19 +0100

    add project skeleton

---
+++ src/main/java/com/smahjoub/cqrses/CqrsesApplication.java
@@ -0,0 +1,13 @@
+package com.smahjoub.cqrses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsesApplication.class, args);
+	}
+
+}
